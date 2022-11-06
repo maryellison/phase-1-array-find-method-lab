fe2b57237871b81513877e1de2febfa74bc63c33
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-11-06 10:51:16 -0500

    commit tree 0b315c198bbabf980f7923c4d21f560f558c2c41

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// code your solution here
+//function superbowlWin(array){
+//    if (array.result === 'W'){
+//        console.log(array.year)
+//    }
+//    console.log(array.find(superbowlWin));
+//}
+
+//function superbowlWin(array){
+//    array.find(obj);
+//    if (obj.result === 'W'){
+//        console.log(obj.result);
+//    }
+//}
+
+//function superbowlWin(record){
+//    let gameWin = record.find(function(obj)){
+//        return obj.result === "W"
+//        }}
+//    console.log(gameWin)
+
+//function superbowlWin(array){
+//   for (const obj of array) {
+//      if (obj.result === "W"){
+//        console.log(obj.year);
+//      }
+//    }
+//}
+
+function superbowlWin(record) {
+    let item = record.find(obj => obj.result === "W")
+    if (item) {
+        return item.year
+    }
+}
+
+superbowlWin(record);
